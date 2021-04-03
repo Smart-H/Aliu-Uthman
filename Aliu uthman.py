dd78653cc3c69ec15f980b636ddd109a188dff53
Smart-H <64838005+Smart-H@users.noreply.github.com>
2021-04-03 01:19:49 +0100

    Add files via upload

---
+++ Aliu uthman.py
@@ -0,0 +1,25 @@
+print("NAME:Aliu Tunde Uthman")
+print("DEPARTMENT: Computer Science")
+scare = "Boo!"
+print (scare)
+var1 = 56
+var2 = 67
+third_score= var1 + var2
+print(var1, var2, third_score)
+var5 = 12
+var4 = .5
+var6 = var5 / var4
+print (var6)
+one = 34
+two = 56
+three = one + two
+print (three)
+dash = 8
+crash = dash % 3
+print (crash)
+A1 = 2
+A2 = A1 * 4
+B1 = 4
+B2 = B1 + 2
+print (A2 * B1 + 2)
+
